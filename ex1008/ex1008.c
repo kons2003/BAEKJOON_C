8131d31735b21e8edde465364f74cb0d49d86cdb
kons2003 <mj030313@naver.com>
2023-03-09 23:59:24 +0900

    문제 주석 추가

--- ex1008/ex1008.c
+++ ex1008/ex1008.c
@@ -1,3 +1,4 @@
+// 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
--- ex1330/ex1330.c
+++ ex1330/ex1330.c
@@ -1,3 +1,4 @@
+// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
