549bc22d0a31414b68c6c816f06d6acb1ad19868
kons2003 <mj030313@naver.com>
2023-04-02 16:53:31 +0900

    꼬마 정민

---
+++ Ex11382/LittleJeongmin.c
@@ -0,0 +1,12 @@
+// ²¿¸¶ Á¤¹Î
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+    double A, B, C;
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    printf("%.0lf", A + B + C);
+
+    return 0;
+}
