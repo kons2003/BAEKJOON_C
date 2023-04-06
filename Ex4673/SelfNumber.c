4de92132c81dd5d088901b15bb1ab8d1482a09fa
kons2003 <mj030313@naver.com>
2023-04-06 18:53:53 +0900

    셀프 넘버

--- Ex4673/SelfNumber.c
+++ Ex4673/SelfNumber.c
@@ -2,8 +2,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+int sum(int n)
+{
+	int sum = n;
+
+	while (n > 0)
+	{
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
+}
+
 int main() {
-	int num, self = 0;
+	int arr[10001] = { 0, };
+	int i, check = 0;
+
+	for (i = 1; i <= 10000; i++)
+	{
+		check = sum(i);
+		if (check <= 10000)
+			arr[check] = 1;
+
+		if (arr[i] != 1)
+			printf("%d\n", i);
+	}
 
 	return 0;
 }
