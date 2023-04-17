d6984d604d19b23fbaf63b868ce4c7fbef4f9cd9
kons2003 <mj030313@naver.com>
2023-04-17 18:12:31 +0900

    음계

---
+++ Ex2920/Scale.c
@@ -0,0 +1,39 @@
+// À½°è
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	//char scale[10] = { ' ', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+	int n[9] = { 0 };
+	int asc = 0, des = 0;
+	int i = 0, j = 0;
+
+	for (i = 1; i <= 8; i++)
+	{
+		scanf("%d", &n[i]);
+	}
+
+	for (i = 1; i <= 8; i++)
+	{
+		if (n[i] == i)
+		{
+			asc++;
+			continue;
+		}
+		else if (8 - n[i] == j)
+		{
+			des++;
+			j++;
+			continue;
+		}
+	}
+
+	if (asc == 8)
+		printf("ascending\n");
+	else if (des == 8)
+		printf("descending\n");
+	else
+		printf("mixed\n");
+
+	return 0;
+}
