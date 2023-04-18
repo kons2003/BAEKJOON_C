0b2b0c93d3725aafe1fd3a9cea9287a93fe42394
kons2003 <mj030313@naver.com>
2023-04-18 16:44:52 +0900

    알파벳 찾기

--- Ex10809/FindTheAlphabet.c
+++ Ex10809/FindTheAlphabet.c
@@ -3,7 +3,26 @@
 #include<stdio.h>
 
 int main() {
-	
+	char s[102] = { 0 };
+	int i, j;
+
+	scanf("%s", &s);
+
+	for (i = 97; i <= 122; i++)
+	{
+		j = 0;
+
+		while (s[j] != 0 )
+		{
+			if (s[j] == (char)i )
+				break;
+			j++;
+		}
+		if (s[j] == (char)i)
+			printf("%d ", j);
+		else
+			printf("%d ", (-1));
+	}
 
 	return 0;
 }
