71993f262e80ae65be9f47240c9b849d9412982f
kons2003 <mj030313@naver.com>
2023-04-26 20:41:44 +0900

    단어 길이 재기

---
+++ Ex2743/MeasureWordLength.c
@@ -0,0 +1,13 @@
+// 단어 길이 재기
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	char arr[101] = "";
+
+	scanf("%s", &arr);
+	printf("%d\n", strlen(arr));
+
+	return 0;
+}
