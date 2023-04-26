83c9e8b97ce90d71d9e27d1ca3e600891660b88a
kons2003 <mj030313@naver.com>
2023-04-27 00:27:50 +0900

    문자열

---
+++ Ex9086/String.c
@@ -0,0 +1,18 @@
+// ¹®ÀÚ¿­
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	int n;
+	char arr[1001] = "";
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s", &arr);
+		printf("%c%c\n", arr[0], arr[strlen(arr) - 1]);
+	}
+
+	return 0;
+}
