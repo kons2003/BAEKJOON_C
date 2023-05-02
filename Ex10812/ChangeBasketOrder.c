1b21d881827a0a8cafdcd7ac0b2e62d557cd7729
kons2003 <mj030313@naver.com>
2023-05-02 23:45:31 +0900

    스택

---
+++ Ex10812/ChangeBasketOrder.c
@@ -0,0 +1,31 @@
+// 바구니 순서 바꾸기
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int n, m, i, j, temp;
+	int beg, mid, end;
+	int* arr;
+
+	scanf("%d %d", &n, &m);
+	arr = malloc(sizeof(int) * n);
+	for (i = 1; i <= n; i++)
+		arr[i] = i;
+	for (i = 0; i < m; i++)
+	{
+		scanf("%d %d %d", &beg, &end, &mid);
+		for (; beg - 1 < mid - 1; mid--, end--)
+		{
+			temp = arr[mid - 2];
+			for (j = mid - 1; j <= end - 1; j++)
+				arr[j - 1] = arr[j];
+			arr[end - 1] = temp;
+		}
+	}
+	for (j = 0; j < n; j++)
+		printf("%d ", arr[j]);
+	free(arr);
+
+	return 0;
+}
---
+++ Ex1316/GroupWordChecker.c
@@ -0,0 +1,8 @@
+// 그룹 단어 체커
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+
+	return 0;
+}
